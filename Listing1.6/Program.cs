961a617aff4830aa31d5bdec46beb43b016fe8e3
Глеб <@DESKTOP-V871R2N>
2024-11-19 20:21:08 +0300

    Add project files.

---
+++ Listing1.6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Dialog
+{
+    static void Main ()
+    {
+        Console.Title = "Давайте познакомимся";
+        Console.Write("Как вас зовут? ");
+        string name = Console.ReadLine();
+        string txt = "Очень приятно, " + name + "!";
+        Console.WriteLine(txt);
+    }
+}
